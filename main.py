735ce5d30b2f15b6bd049aa1463f2bb2d69f4a1b
AlgoHype <134722077+thealgohype@users.noreply.github.com>
2024-05-27 19:47:20 +0000

    updated gitignore

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ from langchain_core.runnables import RunnablePassthrough
 from langchain.prompts import ChatPromptTemplate
 
 # Set your API keys
-openai.api_key = os.environ.get('OPENAI_API_KEY')
+openai.api_key = os.environ['oai']
 lc_key = os.environ['langchain']
 
-#Setup LangSmith for token usage monitoring :
+# Setup LangSmith for token usage monitoring :
 # Setup LANGSMITH for LLMOps :
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
 os.environ["LANGCHAIN_PROJECT"] = "NewsLetter"
@@ -24,7 +24,7 @@ os.environ["LANGCHAIN_API_KEY"] = lc_key
 st.title("RSS Feed Newsletter Aggregator & Rewriter")
 
 llm = ChatAnthropic(model="claude-3-opus-20240229",
-                    api_key=os.environ['claude'])
+                    api_key=os.environ['claude']
 
 with open('prompt.txt', 'r') as file:
     prompt = ChatPromptTemplate.from_template(file.read())
@@ -87,17 +87,7 @@ if st.button("Generate Newsletter"):
         st.write("### Newsletter")
         st.write(rewritten_newsletter)
         if rewritten_newsletter:
-            button = st.button("Download Newsletter",on_click= download_newsletter(rewritten_newsletter))
-            
-            # Download button to save the generated newsletter as a .txt file
-            # def download_newsletter(text):
-            #     with open('rewritten_newsletter.txt', 'w') as file:
-            #         file.write(text)
-            #         st.markdown(f"Download [rewritten_newsletter.txt](rewritten_newsletter.txt)")
-            #     return
-                
+            st.button("Download Newsletter", on_click=download_newsletter, args=(rewritten_newsletter,))
     except Exception as e:
         st.write(f"Failed to generate newsletter: {e}")
         st.stop()
-
-
